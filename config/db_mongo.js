1ceb90535958fac41ac9065f089cb0a4dff9a3ad
luiscacostas <acosta.luisc15@gmail.com>
2024-07-14 17:26:51 +0200

    add new changes config db

--- config/db_mongo.js
+++ config/db_mongo.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectToDatabase = async() => {
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 5000, // Tiempo de espera para selección de servidor
-  socketTimeoutMS: 45000 // Tiempo de espera para el socket
-}
-  
-)}
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
+    console.log('Conectado a la base de datos MongoDB');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos MongoDB:', error);
+    process.exit(1);
+  }
+};
 
-
-module.exports = connectToDatabase;
+module.exports = connectToDatabase
 
