57c97b48a378a9eae7b9f9c7c14c04d9bbeb28a8
luiscacostas <acosta.luisc15@gmail.com>
2024-07-17 21:36:09 +0200

    resolve problems with functions

--- services/monument.services.js
+++ services/monument.services.js
@@ -100,8 +100,7 @@ module.exports = {
   createMonument,
   updateMonument,
   desactivateMonument,
-  captureMonument,
-  getAllMonumentsForUser
+  captureMonument
 };
 
 // const saveMonument = async () => {
