10b1baf5240b66eef59d61bf105c44496a802784
luiscacostas <acosta.luisc15@gmail.com>
2024-07-18 21:23:54 +0200

    add changes middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 // authMiddleware.js
 
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user.models');
 
 const authMiddleware = async (req, res, next) => {
   try {
